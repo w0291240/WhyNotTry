4bee347782a865f8d417a027d86a2fd95e39f2a0
Jesse Crocker <w0291240@nscc.ca>
2026-05-16 00:06:28 -0300

    Initial Commit

---
+++ WhyNotTry/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhyNotTry
+//
+//  Created by Jesse Crocker on 2026-05-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Jesse Crocker on 2026-05-16.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
