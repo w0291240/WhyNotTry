3582ee6c58b94dfee8b5bc73770c70baabc8b614
Jesse Crocker <w0291240@nscc.ca>
2026-05-16 23:30:42 -0300

    Finished tutorial.

--- WhyNotTry/ContentView.swift
+++ WhyNotTry/ContentView.swift
@@ -11,7 +11,11 @@ struct ContentView: View {
     
     var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
     
+    var colors: [Color] = [.blue, .cyan, .green, .yellow, .orange, .red, .purple, .pink, .gray, .black]
+    
     @State private var selected = "Rugby"
+    
+    @State private var id = 1
     
     var body: some View {
         //Text("Hello, SwiftUI!")
@@ -20,21 +24,33 @@ struct ContentView: View {
                 .font(.largeTitle.bold())
         }
         
+        Spacer()
+        
         VStack{
             Circle()
-                .fill(.blue)
+                .fill(colors.randomElement() ?? .blue)
                 .padding()
+            
                 .overlay(
                     Image(systemName:"figure.\(selected.lowercased())")
                         .font(.system(size:144))
                         .foregroundStyle(.white))
+                .transition(.slide)
+                .id(id)
             
             Text("\(selected)!")
                 .font(.title)
         }
         
+        Spacer();
+        
         Button("Try Again") {
-           selected = activities.randomElement() ?? "Rugby";
+            withAnimation(.easeInOut(duration: 1)) {
+                selected = activities.randomElement() ?? "Rugby";
+                id += 1;
+                
+            }
+
         }
         .buttonStyle(.borderedProminent)
         }
